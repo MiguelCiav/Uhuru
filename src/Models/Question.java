3891cd0a92a149d13a36b75c4a9334a09883b452
CaoCarlos55 <cao345511@gmail.com>
2024-02-02 23:10:42 -0400

    Crear Clases Modelos

---
+++ src/Models/Question.java
@@ -0,0 +1,44 @@
+package Models;
+
+public class Question {
+
+    private String description;
+    private String justification;
+    private Answer userAnswer;
+    private int questionType;
+    private int questionNumber;
+
+
+    public Question(){
+    };
+
+    public void addAnswer(Answer answer){};
+    public void answerQuestion(Answer answer){};
+
+    public String getDescription(){
+        return description;
+    }
+
+    public String getJustificacion(){
+        return justification;
+    }
+
+    public Answer getUserAnswer(){
+        return userAnswer;
+    }
+    
+    public int getQuestionType(){
+        return questionType;
+    }
+
+    public int getQuestionNumber(){
+        return questionNumber;
+    }
+
+    public Answer getAnswer(int answerNumber){
+        //Borrador
+        Answer aux = new Answer("Hola", 1, false, answerNumber);
+        return aux;
+    }
+
+}
---
+++ src/Models/Test.java
@@ -0,0 +1,20 @@
+package Models;
+
+public class Test {
+
+    private String type;
+    private int duration;
+    private float calification;
+
+    public Test(String type, int duration){
+
+        this.type = type;
+        this.duration = duration;
+    }
+
+    public void addQuestion(){};
+    public void getQuestion(){};
+
+
+    
+}
