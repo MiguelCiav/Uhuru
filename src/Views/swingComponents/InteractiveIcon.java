6caf8d70204613d171bf7e573a9086b22f3e1085
Miguel Ciavato <miguelciavato@gmail.com>
2024-02-03 02:03:37 -0400

    Label Listener eliminado

--- src/Views/swingComponents/InteractiveIcon.java
+++ src/Views/swingComponents/InteractiveIcon.java
@@ -1,10 +1,10 @@
 package Views.swingComponents;
 
 import java.awt.Dimension;
+import java.awt.event.MouseAdapter;
 import java.awt.event.MouseListener;
 import java.io.File;
 import javax.swing.*;
-import Views.listeners.LabelListener;
 
 // Esta clase se hizo con el fin de crear etiquetas (JLabel) con interacciones
 
@@ -14,7 +14,7 @@ public class InteractiveIcon extends JLabel{
         this.setIcon(new ImageIcon(getClass().getResource(imageURL)));
     }
 
-    InteractiveIcon(String label, String imageURL, LabelListener action){
+    InteractiveIcon(String label, String imageURL, MouseAdapter action){
         this.setIcon(new ImageIcon(getClass().getResource(imageURL)));
         this.setText(label); 
         addMouseListener(action);
