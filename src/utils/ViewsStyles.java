4d183e23ec300b864ff134dbace315ac56219f08
Miguel Ciavato <miguelciavato@gmail.com>
2024-02-04 18:51:44 -0400

    Añadida clase de estilos a main

---
+++ src/utils/ViewsStyles.java
@@ -0,0 +1,15 @@
+package utils;
+
+import java.awt.Color;
+import java.awt.Font;
+
+public class ViewsStyles {
+
+    public static final Color ULTRA_LIGHT_BLUE = new Color(217,233,241);
+    public static final Color LIGHT_BLUE = new Color(152,193,217);
+    public static final Color DARK_BLUE = new Color(61, 90, 128);
+    public static final Font TITLE_FONT = new Font("Futura", Font.BOLD, 32);
+    public static final Font SMALL_TITLE_FONT = new Font("Futura", Font.BOLD, 20);
+    public static final Font TEXT_FONT = new Font("Futura", Font.PLAIN, 12);
+    
+}
