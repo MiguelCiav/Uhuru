3543703d11409c1b3661286b938a0052482b534b
Eri <wholood@gmail.com>
2024-02-04 16:00:15 -0400

    Para no romper todo

--- src/Views/useCaseFrames/TestListView.java
+++ src/Views/useCaseFrames/TestListView.java
@@ -13,17 +13,17 @@ public class TestListView extends JFrame{
 
     private LabelContainer courseContainer;
     private TestListPanel testListPanel = new TestListPanel();
-    //private DoublePanel doublePanel;
+    private DoublePanel doublePanel;
 
     public TestListView (){
         courseContainer = new LabelContainer("Cursos Activos", LabelContainer.COURSES_CONTAINER);
-       // doublePanel = new DoublePanel(courseContainer, testListPanel);
+        doublePanel = new DoublePanel(courseContainer, testListPanel);
 
         setTitle("Uhuru: Available Tests");
         setExtendedState(JFrame.MAXIMIZED_BOTH);
         setMinimumSize(new Dimension(800,600));
         setDefaultCloseOperation(EXIT_ON_CLOSE);
-        add(new JPanelTransparent());
+        add(doublePanel);
 
         setVisible(true);
     }
